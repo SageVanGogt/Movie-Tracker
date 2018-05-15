edb95040221cae7c78bf1d6e30a2b442764ee46f
Sage Vogt <thurmanvogt@gmail.com>
2018-05-15 17:58:47 -0600

    Add addFilmsReducer to rootReducer
    State now updates with apiCall data for films

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import addFilmsReducer from './addFilmsReducer';
 
 const rootReducer = combineReducers({
- 
+  films: addFilmsReducer
 })
 
 export default rootReducer
