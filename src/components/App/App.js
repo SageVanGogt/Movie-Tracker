8dbcf5f80eb9be8a5fdf9be37f80030c0482029f
Sage Vogt <thurmanvogt@gmail.com>
2018-05-15 17:54:38 -0600

    Add mapDispatchToProps to App
    Complete with testing and mockApiCall environment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import fetchFilmData from './../../apiCall/apiCall';
 import cleanFilmData from './../../helper/helper';
-import * as actions from './../../actions/index';
+import { connect } from 'react-redux';
+import { addRecentFilms } from './../../actions/index';
 import './App.css';
 
-class App extends Component {
+export class App extends Component {
   
-  async componentDidMount() {
+  componentDidMount() {
+    this.getRecentFilms()
+  }
+
+  async getRecentFilms() {
     const recentFilms = await fetchFilmData();
-    const pageLoadFilms = cleanFilmData(recentFilms);
-    this.props.addRecentFilms(pageLoadFilms);
+    const pageLoadFilms = cleanFilmData(recentFilms.results);
+    this.props.handlePageLoadFilms(pageLoadFilms);
   }
 
   render() {
@@ -21,8 +26,8 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  addRecentFilms: (pageLoadFilms) => dispatch(action.addRecentFilms(pageLoadFilms))
-}
+export const mapDispatchToProps = (dispatch) => ({
+  handlePageLoadFilms: (pageLoadFilms) => dispatch(addRecentFilms(pageLoadFilms))
+})
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
