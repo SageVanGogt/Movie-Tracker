314a7a2b70f1e5bfd96081b2aa79c3f0ac437b87
David Daugherty <david.m.daugherty@gmail.com>
2018-05-17 16:09:47 -0600

    Create onClick functionality to remove user from store

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,20 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import { logOutUser} from '../../actions';
 
-const Nav = () => {
+const Nav = (props) => {
   return (
     <div>
-      <button>Sign Out</button>
+      <button
+        onClick={props.handleLogout}
+      
+      >Sign Out</button>
     </div>
   )
 }
 
-export default Nav;
+export const mapDispatchToProps = (dispatch) => ({
+  handleLogout: () => dispatch(logOutUser())
+})
+
+export default connect(null, mapDispatchToProps)(Nav);
