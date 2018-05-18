46722c77caeff47b497366a94a75dcc4b3eb03a1
Alan Charles <alanjcharles14@gmail.com>
2018-05-17 19:00:52 -0600

    Add Card snapshot

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -3,7 +3,17 @@ import {shallow} from 'enzyme'
 import Card from "./Card"
 
 describe('Card', () => {
-  let wrapper = shallow(<Card />)
-
-  expect(wrapper).toMatchSnapshot()
+  it('should match the snapshot', () => {
+    let mockProps = {
+        id: 1,
+        vote_average: 34,
+        title: "Fury",
+        poster_path: "www.theworstmovieever.com",
+        overview: "a bunch of text about how bad this move is",
+        release_date: 2014
+      }
+    let wrapper = shallow(<Card {...mockProps}/>)
+  
+    expect(wrapper).toMatchSnapshot()
+  })
 });
