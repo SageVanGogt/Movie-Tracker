1721c3dc37ddd65aba309a0e882b87a6fc3b7aa5
David Daugherty <david.m.daugherty@gmail.com>
2018-05-21 18:21:13 -0600

    Add errorReducer and test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import addFilmsReducer from './addFilmsReducer';
 import updateStoreUserReducer from './updateStoreUserReducer'
 import addFavoritesReducer from './addFavoritesReducer'
+import errorReducer from './errorReducer';
 
 const rootReducer = combineReducers({
   films: addFilmsReducer,
   user: updateStoreUserReducer,
-  favorites: addFavoritesReducer
+  favorites: addFavoritesReducer,
+  error: errorReducer
 })
 
 export default rootReducer
